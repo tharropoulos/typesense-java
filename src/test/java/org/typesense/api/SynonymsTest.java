9d3ffd31aae2b0952e5bf7ed09c9bc1488ccd801
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-11-08 15:47:48 +0200

    test(synonyms): enhance synonym tests with assertions
    * add comprehensive field assertions for all synonym operations
    * remove redundant println statements and comments

--- src/test/java/org/typesense/api/SynonymsTest.java
+++ src/test/java/org/typesense/api/SynonymsTest.java
@@ -1,9 +1,14 @@
 package org.typesense.api;
 
 import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.typesense.model.SearchSynonym;
+import org.typesense.model.SearchSynonymDeleteResponse;
 import org.typesense.model.SearchSynonymSchema;
+import org.typesense.model.SearchSynonymsResponse;
 
 class SynonymsTest {
 
@@ -28,25 +33,56 @@ class SynonymsTest {
     void testUpsert() throws Exception {
         SearchSynonymSchema synonym = new SearchSynonymSchema();
 
-        //One-way
+        // One-way
         synonym.addSynonymsItem("dictionary").addSynonymsItem("guide").addSynonymsItem("encyclopedia");
         synonym.root("books");
 
-        System.out.println(this.client.collections("books").synonyms().upsert("books-synonyms", synonym));
+        SearchSynonym result = this.client.collections("books").synonyms().upsert("books-synonyms", synonym);
+
+        assertNotNull(result);
+
+        assertEquals(3, result.getSynonyms().size());
+        assertEquals(synonym.getSynonyms(), result.getSynonyms());
     }
 
     @Test
     void testRetrieve() throws Exception {
-        System.out.println(this.client.collections("books").synonyms("coat-synonyms").retrieve());
+        SearchSynonymSchema synonym = new SearchSynonymSchema();
+        synonym.addSynonymsItem("blazer").addSynonymsItem("coat").addSynonymsItem("jacket");
+
+        SearchSynonym result = this.client.collections("books").synonyms("coat-synonyms").retrieve();
+
+        assertNotNull(result);
+
+        assertEquals(3, synonym.getSynonyms().size());
+        
+        assertEquals("blazer", synonym.getSynonyms().get(0));
+        assertEquals("coat", synonym.getSynonyms().get(1));
+        assertEquals("jacket", synonym.getSynonyms().get(2));
     }
 
     @Test
     void testRetrieveAll() throws Exception {
-        System.out.println(this.client.collections("books").synonyms().retrieve());
+        SearchSynonymsResponse result = this.client.collections("books").synonyms().retrieve();
+
+        assertNotNull(result);
+
+        assertEquals(1, result.getSynonyms().size());
+
+        SearchSynonym synonym = result.getSynonyms().get(0);
+
+        assertEquals("coat-synonyms", synonym.getId());
+        assertEquals(3, synonym.getSynonyms().size());
+        
+        assertEquals("blazer", synonym.getSynonyms().get(0));
+        assertEquals("coat", synonym.getSynonyms().get(1));
+        assertEquals("jacket", synonym.getSynonyms().get(2));
     }
 
     @Test
     void testDelete() throws Exception {
-        System.out.println(this.client.collections("books").synonyms("coat-synonyms").delete());
+        SearchSynonymDeleteResponse result = this.client.collections("books").synonyms("coat-synonyms").delete();
+
+        assertEquals("coat-synonyms", result.getId());
     }
 }
